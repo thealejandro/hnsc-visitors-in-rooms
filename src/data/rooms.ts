952ae1a38ef135b6307b2aefc36f82b1cf972782
thealejandro <thealejandro7w7@gmail.com>
2024-06-27 15:49:24 -0600

    Add properties for data

---
+++ src/data/rooms.ts
@@ -0,0 +1,109 @@
+interface Room {
+    number: number;
+    patientName: string;
+    age: number;
+    visitors: number;
+    sex: string;
+    treatment: string;
+  }
+  
+  export const rooms: Room[] = [
+    {
+      number: 2,
+      patientName: 'John Doe',
+      age: 45,
+      visitors: 3,
+      sex: 'Male',
+      treatment: 'Surgery'
+    },
+    {
+      number: 3,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    {
+      number: 4,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    {
+      number: 5,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    {
+      number: 6,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    {
+      number: 7,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    {
+      number: 8,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    {
+      number: 9,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    {
+      number: 11,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    {
+      number: 12,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    {
+      number: 13,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    {
+      number: 14,
+      patientName: 'Jane Smith',
+      age: 30,
+      visitors: 2,
+      sex: 'Female',
+      treatment: 'Pediatrics'
+    },
+    // Más habitaciones...
+  ];
+  
